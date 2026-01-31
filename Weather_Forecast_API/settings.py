808f7d0c5beba166eeffb084297aa9be27552de6
Monika <monikanimmana3366@example.com>
2026-01-31 15:52:20 +0530

    Add varibles in portfolio models

--- Weather_Forecast_API/settings.py
+++ Weather_Forecast_API/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+WEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

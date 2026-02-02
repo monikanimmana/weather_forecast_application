b2a0f7befbcf26950b99508be9e8d050c0864635
Monika <monikanimmana3366@example.com>
2026-02-02 10:06:18 +0530

    Send main logic of weather

--- Weather_Forecast/models.py
+++ Weather_Forecast/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Weather_Details(models.Model):
+    city=models.TextField()
+
+    def __str__(self):
+        return self.city
--- Weather_Forecast/views.py
+++ Weather_Forecast/views.py
@@ -1,42 +1,76 @@
 from django.shortcuts import render
 import requests
 from django.conf import settings
-from django.http import JsonResponse
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
 def get_weather_details(request):
-    city = request.GET.get("city")
+        if request.method == "GET":
+             
+            city_name = request.GET.get('city')
 
-    if not city:
-        return JsonResponse(
-            {"error":"City name is required"},
-            status=400
-        )
+            if not city_name:
+                return render(request , 'index.html',{
+                    "error":"City name does not exist"
+                })
+
+            url = (
+                f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={settings.WEATHER_API_KEY}&units=metric"
+            )
+
+            response = requests.get(url)
+
+            if response.status_code != 200:
+                return render(request , 'weather_report.html', {
+                     "error":" Please enter a vaild city name"
+                })
+            
+        
+            data = response.json()
+
+            fetch_data ={
+                    "city_name": data["name"],
+                    "temperature" : data["main"]["temp"],
+                    "humidity" : data["main"]["humidity"],
+                    "condition" :data["weather"][0]["description"],
+                    "wind_speed":data["wind"]["speed"]
+                }
+
+            return render(request, 'weather_report.html' , {
+                    'fetch_data' : fetch_data
+                })
+        
+        return render(request,'index.html')
+            
+        
+        
+
+        
     
-    url = (
-        f"https://api.openweathermap.org/data/2.5/weather"
-        f"?q={city}&appid={settings.WEATHER_API_KEY}&units=metric"
-    )
-
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        return JsonResponse(
-            {"error":"City not found"},
-            status = 404
-        )
+         
     
-    data = response.json()
 
-    fetch_data ={
-        "city_name": data["name"],
-        "temperature" : data["main"]["temp"],
-        "humidity" : data["main"]["humidity"],
-        "weather" :data["weather"][0]["icon"],
-        "wind_speed":data["wind"]["speed"]
-    }
+    
 
-    return JsonResponse(fetch_data)
+
+    
+        
+        
+    # error = "City name is required"
+
+    # if not city_name:
+    #     return response(request,'index.html',
+    #         {"error":error},
+    #         status=400
+    #     )
+    
+    
+
+    
+    # return response(request,'weather_report.html',{'fetch_data':fetch_data})
 
 
 

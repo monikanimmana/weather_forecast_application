0896ba6b09acdb26ea68be903755d7bf1af2dff2
Monika <monikanimmana3366@example.com>
2026-02-02 11:32:26 +0530

    Prepare project for deployment

--- Weather_Forecast_API/settings.py
+++ Weather_Forecast_API/settings.py
@@ -28,8 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'SECRET_KEY'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG') == 'True'
+OPENWEATHER_API_KEY = os.environ.get('OPENWEATHER_API_KEY')
 
 ALLOWED_HOSTS = ['*']
 

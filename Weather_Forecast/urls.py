d1420d2409311c38176157dcf687a2d581727526
Monika <monikanimmana3366@example.com>
2026-02-04 15:25:20 +0530

    Initial commit

--- Weather_Forecast/urls.py
+++ Weather_Forecast/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index,name="index"),
-    path('weather_report/',get_weather_details,name="weather_report"),
+    path('',get_weather_details,name="weather"),
     
 ]
